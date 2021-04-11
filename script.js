2262fae633352bbbe8be2bfb931ee00e6eabe169
cmiciano <35476727+cmiciano@users.noreply.github.com>
2021-04-11 00:13:55 -0700

    displaying messages script

--- script.js
+++ script.js
@@ -2,28 +2,45 @@
     to your site with Javascript */
 
 // prints "hi" in the browser's dev tools console
-console.log("hi");
 
-var content = document.getElementById("content");
-content.addEventListener("click", function(){
-    var width1 = content.offsetWidth
-    var height1 = content.offsetHeight
-    content.style.width= (width1+200)+"px";
-    content.style.height= (height1+200)+"px";
+let query = window.location.search;
+console.log(query)
 
-
-var content = document.getElementById("content1");
-content.addEventListener("click", function(){
+let url_params = new URLSearchParams(query);
+let a = url_params.get("a")
+console.log(a)
 
 
-    window.setTimeout(function(){
+let card_desc = [
+    "test_string1",
+    "test_string2",
+    "test_string3",
+    "test_string4",
+    "test_string5",
+    "test_string6",
+    "test_string7",
+    "test_string8",
+    "test_string9",
+    "test_string10",
+    "test_string11",
+    "test_string12",
+    "test_string13",
+    "test_string14",
+    "test_string15",
+    "test_string16",
+    "test_string17",
+    "test_string18",
+    "test_string19",
+    "test_string20"
 
-        // Move to a new location or you can do something else
-        //window.location.href = "https://www.google.com";
-        window.location.href = "PostItMessage.html"
-
-    }, 1000);
-
-});
+]
+let index_val = parseInt(a)
 
 
+document.getElementById("resourceHeadingText").innerText = card_desc[index_val]
+try {
+    document.getElementById("resourceHeadingText-1").innerText = card_desc[index_val + 1]
+    document.getElementById("resourceHeadingText-2").innerText = card_desc[index_val + 2]
+    document.getElementById("resourceHeadingText-3").innerText = card_desc[index_val + 3]
+}
+catch (error) {}
